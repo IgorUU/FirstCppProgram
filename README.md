c4c0b40dce6dee161958f9a0f923e4fdbc448101
Igor <igor@studiopresent.com>
2023-11-05 18:17:14 +0100

    Input/Output

--- README.md
+++ README.md
@@ -204,5 +204,29 @@ John Doe 32
 Hello John! You are 0 years old.
 ```
 
+We can solve that problem by using **getline()** function.
+
+```
+int age;
+std::string full_name;
+
+std::cout << "Type in your full name: " << std::endl;
+std::getline(std::cin, full_name);
+
+std::cout << "Type in your age: " << std::endl;
+std::cin >> age;
+std::cout << "Hello " << full_name << "! You are " << age << " years old." << std::endl;
+```
+
+And the output would be:
+
+```
+Type in your full name:
+Igor Usumovic
+Type in your age:
+32
+Hello Igor Usumovic! You are 32 years old.
+```
+
 
 
--- main.cpp
+++ main.cpp
@@ -9,11 +9,14 @@ int addNumbers(int first, int second) {
 int main(int argc, char **argv) {
 
   int age;
-  std::string name;
+  std::string full_name;
 
-  std::cout << "Type in your last name and age, separated by spaces: " << std::endl;
-  std::cin >> name >> age;
-  std::cout << "Hello " << name << "! You are " << age << " years old." << std::endl;
+  std::cout << "Type in your full name: " << std::endl;
+  std::getline(std::cin, full_name);
+
+  std::cout << "Type in your age: " << std::endl;
+  std::cin >> age;
+  std::cout << "Hello " << full_name << "! You are " << age << " years old." << std::endl;
 
 
   return 0;
